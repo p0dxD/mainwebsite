a7a140c130c8677db116e931c9e29c875a97a6b2
Jose Rodriguez <jose0797@gmail.com>
2017-11-16 15:10:30 -0500

    added profile pic

---
+++ app/assets/js/main.js
@@ -0,0 +1,13 @@
+$(document).foundation()
+
+$(function() {
+  $(window).scroll(function() {
+    var winTop = $(window).scrollTop();
+    if (winTop >= 30) {
+      $("body").addClass("sticky-shrinknav-wrapper");
+    } else{
+      $("body").removeClass("sticky-shrinknav-wrapper");
+    }
+  });
+});
+
