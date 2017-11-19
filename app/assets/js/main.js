03c38387d37c8059a30c826f4ab6fb3415ed15fd
Jose Rodriguez <jose0797@gmail.com>
2017-11-18 21:01:08 -0500

    added search functionality to projects

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -25,5 +25,55 @@ $(document).ready(function() {
         $("main").load("projects/projects.html .maincontent");
         $('body').css('overflow','scroll');
     });
+
 });
 
+function search(ele) {
+    if(event.key === 'Enter') {
+      var searchValue = ele.value;
+    $.getJSON("assets/json/projects.json", function(json) {
+      var projects = json.projects;
+      var sizeProjects = json.projects.length;
+       var test = "";
+      // var listOfItems = [];
+      for(var step = 0; step < sizeProjects; step++){
+        var currentProject = projects[step];
+        for (var key in currentProject) {
+        // check if the property/key is defined in the object itself, not in parent
+        if (currentProject.hasOwnProperty(key)) {           
+             if(containsWord(currentProject[key],searchValue)){
+              //display the elements that were searched 
+              //for if it contains keyword somewhere
+
+
+      var tmp = `<div class="cell">
+      <div class="card">
+        <div class="card-image">
+          <img src=../assets/images/gifs/`+String(currentProject.src)+`>
+        </div>
+        <div class="card-section">
+          <h4>`+String(currentProject.title)+`</h4>
+          <p>It has an easy to override visual style, and is appropriately subdued.</p>
+        </div>
+      </div>
+    </div>`
+
+            test += tmp;
+              console.log(test);
+              // listOfItems.push(currentProject);
+              break;
+            }//end if
+          }//end if
+        }//end for
+
+      }//end for
+      console.log(test);
+      $( "div.projects").html(test);
+      // console.log(listOfItems);
+    });      
+    }
+}
+
+function containsWord(word, wordToCheck){
+  return word.includes(wordToCheck);
+}
--- gulpfile.js
+++ gulpfile.js
@@ -44,12 +44,13 @@ gulp.task('watch',['browserSync', 'nunjucks', 'styles','move-assets'],function()
     gulp.watch('app/assets/scss/**/*.scss',['styles']);
     gulp.watch('app/assets/css/**/*.css',['css']);
     gulp.watch('app/assets/js/**/*.js',['js']);
+    gulp.watch('app/assets/json/**/*.json',['json']);
     gulp.watch('app/assets/fonts/**/*',['fonts']);
     gulp.watch('app/**/**/*.+(html|njk)',['nunjucks']);
 });
 
 //movefiles
-gulp.task('move-assets', ['css','fonts','js','images','materialize']);
+gulp.task('move-assets', ['css','fonts','js','json','images','materialize']);
 
 //server sync
 gulp.task('browserSync', function() {
@@ -64,29 +65,52 @@ gulp.task('browserSync', function() {
 gulp.task('fonts', function() {
   return gulp.src('app/assets/fonts/**/*')
   .pipe(gulp.dest('build/assets/fonts'))
+  .pipe(browserSync.reload({
+          stream: true
+  }))
 });
 
 //moving css
 gulp.task('css', function() {
   return gulp.src('app/assets/css/**/*')
   .pipe(gulp.dest('build/assets/css'))
+  .pipe(browserSync.reload({
+    stream: true
+  }))  
 });
 
 //moving js
 gulp.task('js', function() {
   return gulp.src('app/assets/js/**/*')
   .pipe(gulp.dest('build/assets/js'))
+        .pipe(browserSync.reload({
+          stream: true
+        }))
+});
+//moving json
+gulp.task('json', function() {
+  return gulp.src('app/assets/json/**/*')
+  .pipe(gulp.dest('build/assets/json'))
+        .pipe(browserSync.reload({
+          stream: true
+        }))
 });
 
 //moving images
 gulp.task('images', function() {
   return gulp.src('app/assets/images/**/*')
   .pipe(gulp.dest('build/assets/images'))
+        .pipe(browserSync.reload({
+          stream: true
+        }))
 });
 
 gulp.task('materialize', function() {
   return gulp.src('app/assets/materialize/**/*')
   .pipe(gulp.dest('build/assets/materialize'))
+        .pipe(browserSync.reload({
+          stream: true
+        }))
 });
 
 // 68796P
