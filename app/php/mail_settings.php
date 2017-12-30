3656ebae7f002c75ce2ac8e34488b0a9578dc49f
Jose Rodriguez <jose0797@gmail.com>
2017-12-29 23:12:06 -0500

    added form

--- app/php/mail_settings.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$email_to = "contact@joserod.me"; // your email address send TO
-$email_from = "contact@joserod.me"; // your email address send FROM
-$email_subject = "Contact form message"; 
-$thankyou = "thankyou.htm"; 
-
-?>
